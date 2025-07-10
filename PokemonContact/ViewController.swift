6ff63b404b6606b7448a894c1a93286a01abf78d
SuYongJung <jisy1376@gmail.com>
2025-07-10 11:23:09 +0900

    Initial Commit

---
+++ PokemonContact/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonContact
+//
+//  Created by 정수용 on 7/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
